fa95d0763c407ae20d981a72286be37333328828
andrewplane <andrew@lanemultimedia.com>
2024-11-19 19:08:10 -0800

    primary key revision

--- EmployeeSQL/tables_schemata.sql
+++ EmployeeSQL/tables_schemata.sql
@@ -1,16 +1,28 @@
+--Reset Tables as needed
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+--Create Tables to match the CSV files (6) provided
 CREATE TABLE departments (
   dept_no VARCHAR(4) NOT NULL,
-  dept_name VARCHAR(30)
+  dept_name VARCHAR(30),
+  PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE dept_emp (
   emp_no int NOT NULL,
-  dept_no VARCHAR(4) NOT NULL
+  dept_no VARCHAR(4),
+  PRIMARY KEY (emp_no, dept_no) --composite PK needed as some employees are in multiple departments
 );
 
 CREATE TABLE dept_manager (
   dept_no VARCHAR(4) NOT NULL,
-  emp_no int NOT NULL
+  emp_no int,
+  PRIMARY KEY (dept_no, emp_no) --composite PK needed as some employees are in multiple departments
 );
 
 CREATE TABLE employees (
@@ -20,15 +32,18 @@ CREATE TABLE employees (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   sex VARCHAR(1),
-  hire_date date
+  hire_date date,
+  PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE salaries (
   emp_no int NOT NULL,
-  salary int
+  salary money,
+  PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE titles (
   title_id VARCHAR(5) NOT NULL,
-  title VARCHAR(30)
+  title VARCHAR(30),
+  PRIMARY KEY (title_id)
 );
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # sql-challenge
 Module 9 Challenge
+
+///
+Andrew Lane
+///
+
+11/19/2024 - Setup GitHub and file structure. Began work on challenge. 
+
+-Used class examples for reference and Xpert for troubleshooting when stuck. 
+
+-Line 13 from the queries.sql file was derived by asking multiple questions to Xpert on how to extract the year value from the date. I was initially using WHERE YEAR(hire_date) = 1986 instead of the needed syntax for postgreSQL of: WHERE EXTRACT(YEAR FROM hire_date) = 1986
+
+-Rebuilt DB and each table with revised primary keys
+
