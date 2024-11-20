4f63182f8ce730843618963ca14d3c416318b6f3
andrewplane <andrew@lanemultimedia.com>
2024-11-19 21:12:15 -0800

    Diagram Update

--- EmployeeSQL/tables_schemata.sql
+++ EmployeeSQL/tables_schemata.sql
@@ -1,28 +1,22 @@
 --Reset Tables as needed
-DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS dept_manager;
-DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS titles;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
 
 --Create Tables to match the CSV files (6) provided
+CREATE TABLE titles (
+  title_id VARCHAR(5) NOT NULL,
+  title VARCHAR(30),
+  PRIMARY KEY (title_id)
+);
+
 CREATE TABLE departments (
   dept_no VARCHAR(4) NOT NULL,
   dept_name VARCHAR(30),
   PRIMARY KEY (dept_no)
-);
-
-CREATE TABLE dept_emp (
-  emp_no int NOT NULL,
-  dept_no VARCHAR(4),
-  PRIMARY KEY (emp_no, dept_no) --composite PK needed as some employees are in multiple departments
-);
-
-CREATE TABLE dept_manager (
-  dept_no VARCHAR(4) NOT NULL,
-  emp_no int,
-  PRIMARY KEY (dept_no, emp_no) --composite PK needed as some employees are in multiple departments
 );
 
 CREATE TABLE employees (
@@ -33,17 +27,29 @@ CREATE TABLE employees (
   last_name VARCHAR(30),
   sex VARCHAR(1),
   hire_date date,
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no),
+  FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE dept_manager (
+  dept_no VARCHAR(4) NOT NULL,
+  emp_no int,
+  PRIMARY KEY (dept_no, emp_no),
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_emp (
+  emp_no int NOT NULL,
+  dept_no VARCHAR(4),
+  PRIMARY KEY (emp_no, dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE salaries (
   emp_no int NOT NULL,
   salary money,
-  PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE titles (
-  title_id VARCHAR(5) NOT NULL,
-  title VARCHAR(30),
-  PRIMARY KEY (title_id)
+  PRIMARY KEY (emp_no),
+  FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
